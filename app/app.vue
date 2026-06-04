6a9b5f45556ba867583723bfc33ef24209647955
Jack Waghorn <jack_waghorn@hotmail.co.uk>
2026-06-04 14:42:46 +0100

    update

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
 <template>
 <NuxtPage />
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'Happy Folke Gardens',
+  meta: [
+    { name: 'description', content: 'Coming soon' },
+  ],
+})
+</script>
